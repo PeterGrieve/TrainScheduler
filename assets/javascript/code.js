c00508f20e64f4bedce80d6a539f319645b594c0
unknown <petergrieve33@gmail.com>
2019-02-20 18:43:45 -0500

    Edited Comments

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -18,6 +18,7 @@ $(document).ready(function () {
 
 });
 
+// This function fetches information from the database, and displays it
 function renderTable() {
 
   $("#trainInfo").empty();
@@ -29,15 +30,27 @@ function renderTable() {
   // Enable logging across page refreshes
   firebase.database.enableLogging(true, true);
 
+  // Firebase's built-in "once" function returns all the information in
+  // the associated database as it is in that moment. It does not listen for 
+  // any new changes to the database
   database.once("value")
+
+    // The snapshot contains all the data from the database, which
+    //can be divided into children snapshots for each train
     .then(function (snapshot) {
 
-      snapshot.forEach(function (childSnapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        // Every train has a unique key associated with it
         var key = childSnapshot.key;
+
         // childData will be the actual contents of the child
         var trainName = childSnapshot.child("name").val();
         var destination = childSnapshot.child("destination").val();
+
+        // The time of the train's first departure for that day 
         var first = childSnapshot.child("first").val();
+
+        // How often the train appears at the station
         var frequency = childSnapshot.child("frequency").val();
 
         console.log(key);
@@ -46,27 +59,33 @@ function renderTable() {
         console.log(firstFormatted);
         console.log(frequency);
 
+        // moment.js is an npm package that neatly formats time displays
         var firstFormatted = moment(first, 'h:mm a');
         var timeDifference = moment().diff(moment(firstFormatted), "minutes");
         var remainder = timeDifference % frequency;
-
         var minutesAway = frequency - remainder;
+
+        // The associated train's next time of arrival
         var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm A");
 
+        // Create a new HTML table row
         var tr = $("<tr>");
 
+        // Create new HTML tables columns for the relevant data
         var trainNameTd = $("<td>").text(trainName);
         var destinationTd = $("<td>").text(destination);
         var frequencyTd = $("<td>").text(frequency);
         var nextArrivalTd = $("<td>").text(nextArrival);
         var minutesAwayTd = $("<td>").text(minutesAway);
 
+        // Append the columns to the row
         tr.append(trainNameTd);
         tr.append(destinationTd);
         tr.append(frequencyTd);
         tr.append(nextArrivalTd);
         tr.append(minutesAwayTd);
 
+        // Append the row to the table
         $("#trainInfo").append(tr);
 
       });
@@ -77,6 +96,8 @@ function renderTable() {
   $("#submitBtn").click(handleSubmitClick);
 }
 
+// This function adds a new train to the database when the 
+// submit button is clicked.
 function handleSubmitClick() {
 
   var trainName = $("#trainName").val();
@@ -86,9 +107,11 @@ function handleSubmitClick() {
 
   console.log(trainName);
 
+  // Create a new unique key for the new train
   var code = Math.floor(Math.random() * 100000);
   var newKey = "train" + code;
 
+  // Write the new train info to the database
   firebase.database().ref('/' + newKey).set({
     name: trainName,
     destination: destination,
@@ -96,11 +119,13 @@ function handleSubmitClick() {
     frequency: frequency
   });
 
+  // Reset the info boxes 
   $("#trainName").empty();
   $("#destination").empty();
   $("#firstTrain").empty();
   $("#frequency").empty();
 
+  // The table must be rendered again to include the new data
   renderTable();
 
 }
